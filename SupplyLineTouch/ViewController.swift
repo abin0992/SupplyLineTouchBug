e76adad870044ea7b7052d25ca5c87b894297a52
Abin <abin.baby@riskmethods.net>
2020-01-29 23:14:02 +0100

    initial commit

--- SupplyLineTouch/ViewController.swift
+++ SupplyLineTouch/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let places = Place.getPlaces()
+    var mapTap: UITapGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addAnnotations()
+        mapTap = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped(tap:)))
+        mapTap.numberOfTapsRequired = 1
+        mapView.addGestureRecognizer(mapTap)
     }
 
+    func addAnnotations(){
+        mapView?.delegate = self
+           mapView?.addAnnotations(places)
+        
+           let overlays = places.map { MKCircle(center: $0.coordinate, radius: 100) }
+           mapView?.addOverlays(overlays)
+           
+           var locations = places.map { $0.coordinate }
+           let polyline = MKPolyline(coordinates: &locations, count: locations.count)
+        mapView?.addOverlay(polyline)
+    }
 
+    @IBAction func mapViewTapped(tap: UITapGestureRecognizer) {
+        let touchPoint: CGPoint = tap.location(in: mapView)
+	           let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+           let mapPoint = MKMapPoint(touchCoordinate)
+
+           for overlay in mapView.overlays {
+               if overlay is MKPolyline {
+                   if let polylineRenderer = mapView.renderer(for: overlay) as? MKPolylineRenderer {
+                       let polylinePoint = polylineRenderer.point(for: mapPoint)
+
+                       if polylineRenderer.path.contains(polylinePoint) {
+                           print("polyline was tapped")
+                       }
+                   }
+               }
+           }
+    }
+
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.orange
+            renderer.lineWidth = 6
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
 }
 
